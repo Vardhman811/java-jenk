9707f58398e1e461bf3a0bc1718560a2ef41bbb8
Vardhman811 <52546204+Vardhman811@users.noreply.github.com>
2024-06-14 18:12:53 +0530

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime;    
-public class CurrentDateTimeExample1 {    
+public class Main {    
   public static void main(String[] args) {    
    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
    LocalDateTime now = LocalDateTime.now();  
